7d1a25e212821efb4ab5af09b473eb46d5a6fdb0
Gavi Narra <gavi.narra@iprocedures.com>
2021-10-26 13:02:07 -0400

    More cleanup from sample code

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,6 @@ namespace SameGame
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<DataStore>();
         }
 
