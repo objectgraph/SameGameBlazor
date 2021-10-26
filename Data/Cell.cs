78992509c19925118c723c440496f3e77ad62906
Gavi Narra <gavi.narra@iprocedures.com>
2021-10-26 15:17:34 -0400

    Testing .NET 5

--- Data/Cell.cs
+++ Data/Cell.cs
@@ -1,3 +1,18 @@
 namespace SameGame.Data{
-    public record struct Cell(int Row, int Col, string Color,bool Selected,bool Deleted);   
+    public class Cell{
+        public int Row{get;set;}
+        public int Col{get;set;}
+        public string Color{get;set;}
+        public bool Selected{get;set;}
+        public bool Deleted{get;set;}
+
+        public Cell(int row, int col, string color, bool selected, bool deleted){
+            this.Row= row;
+            this.Col = col;
+            this.Color = color;
+            this.Selected = selected;
+            this.Deleted = deleted;
+        }
+    }
+       
 }
