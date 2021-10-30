06fccc75c5b5f822d51ab632b4326cf82c3570b6
Gavi Narra <gavi.narra@objectgraph.com>
2021-10-30 19:41:49 -0400

    more cleanup

--- Data/DataStore.cs
+++ Data/DataStore.cs
@@ -2,7 +2,6 @@
 using System.Collections.Concurrent;
 namespace SameGame.Data{
     public class DataStore{
-        public static ObservableConcurrentDictionary<string,Cell[,]> Data=new ObservableConcurrentDictionary<string, Cell[,]>();
         public static ObservableConcurrentDictionary<string,GameState> GlobalStore=new ObservableConcurrentDictionary<string, GameState>();
 
     }
