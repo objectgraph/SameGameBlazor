86648c718c663e2291b8888f3fc5b0d5c0ae591e
Gavi Narra <gavi.narra@iprocedures.com>
2021-11-02 14:40:25 -0400

    Proper cleanup of Event handlers

--- Data/DataStore.cs
+++ Data/DataStore.cs
@@ -4,6 +4,5 @@ namespace SameGame.Data{
     public class DataStore{
         public static ObservableConcurrentDictionary<string,GameState> GlobalStore=new ObservableConcurrentDictionary<string, GameState>();
         public static ObservableConcurrentDictionary<string,Board> GameBoard=new ObservableConcurrentDictionary<string, Board>();
-
     }
 }
